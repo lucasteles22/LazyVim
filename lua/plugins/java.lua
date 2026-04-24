34b62ef2b0277e3ffd36524c070b27fcda81deed
Lucas Teles <lucasteles22@gmail.com>
2026-04-24 20:39:03 -0300

    add other java settings

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -4,12 +4,45 @@ return {
     opts = {
       settings = {
         java = {
+          -- Build
+          autobuild = { enabled = true },
+          configuration = {
+            updateBuildConfiguration = "automatic",
+          },
+
+          -- Completion
           completion = {
+            enabled = true,
             importOrder = {
               "",
               "javax",
               "java",
               "#",
+            },
+          },
+
+          -- Format
+          format = { enabled = true },
+
+          -- Type hierarchy
+          typeHierarchy = { lazyLoad = true },
+
+          -- Null analysis
+          compile = {
+            nullAnalysis = { mode = "automatic" },
+          },
+
+          -- Lombok
+          jdt = {
+            ls = {
+              lombokSupport = { enabled = true },
+            },
+          },
+
+          -- Debug
+          debug = {
+            settings = {
+              forceBuildBeforeLaunch = false,
             },
           },
         },
